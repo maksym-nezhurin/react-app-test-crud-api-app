0e8030215de8b598bd460dba4aa16b11d12c54ee
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 10:23:12 +0200

    Setup CD for Vite project

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-app-test-crud-api-app/',  // Replace with your GitHub repository name
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
