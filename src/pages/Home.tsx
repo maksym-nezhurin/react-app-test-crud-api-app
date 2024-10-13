127a9faec9e249a13f4a970a80bbc2669b1902ed
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 13:06:27 +0200

    Add prod env and fix some errors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Login from '../components/Login';
+import Login from '../../components/Login';
 import axios from 'axios';
 
 interface IArticle {
