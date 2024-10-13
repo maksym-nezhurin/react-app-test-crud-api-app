65d15a82925569a8d52198d67f32b763b3cad08e
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 17:22:22 +0200

    configure vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import react2 from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
-  plugins: [react()],
+  plugins: [react(), react2()],
 });
