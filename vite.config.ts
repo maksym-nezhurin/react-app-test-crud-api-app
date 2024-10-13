4a5b5d022c89d6ad530506c3691c13a3f9df7d78
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 17:20:02 +0200

    configure vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // Conditional base path for production vs. development
-  base: '/react-app-test-crud-api-app/'
-  // base: import.meta.env.VITE_NODE_ENV === 'production' 
-  //   ? '/react-app-test-crud-api-app/' 
-  //   : '/',
+  base: '/react-app-test-crud-api-app/',
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [react()],
 });
