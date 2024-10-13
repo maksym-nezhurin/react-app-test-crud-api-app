d11fabfec17c3f9b725db84c9642c941404a27c0
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-13 18:11:12 +0200

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import react2 from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
