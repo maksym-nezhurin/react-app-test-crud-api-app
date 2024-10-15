3b583ceeab88501093143982ecf4b04e7ff99c39
Maksym Nezhurin <mnezhurin17@gmail.com>
2024-10-15 13:23:44 +0200

    New updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Conditional base path for production vs. development
-  base: '/',
+  base: '/react-app-test-crud-api-app',
   build: {
     outDir: 'dist'
   },
